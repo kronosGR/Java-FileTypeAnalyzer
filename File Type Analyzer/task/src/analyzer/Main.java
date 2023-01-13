8afe4aeb8d80646c5f3ab5b2da40fe443165099e
KronosGR <geo.elgeo@gmail.com>
2023-01-13 11:36:16 +0100

    stage 1/5 completed

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -1,7 +1,31 @@
 package analyzer;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String file = args[0];
+        String pattern = args[1];
+        String resString = args[2];
+
+
+        try(InputStream in = new FileInputStream(file);) {
+            byte[] bytesRead = in.readAllBytes();
+            String tmp = new String(bytesRead, Charset.defaultCharset());
+            if (tmp.contains(pattern)) {
+                System.out.println(resString);
+            } else {
+                System.out.println("Unknown file type");
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
