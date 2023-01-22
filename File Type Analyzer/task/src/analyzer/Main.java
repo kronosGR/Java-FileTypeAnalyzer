28ebf967dc5ed4529d01e0d5ca694e60eb7fc74a
KronosGR <geo.elgeo@gmail.com>
2023-01-22 12:08:37 +0100

    stage 4/5 completed loading the patterns and results from file

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -1,19 +1,16 @@
 package analyzer;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static int THREADS = 5;
     public static void main(String[] args) {
 
         String path = args[0];
@@ -22,16 +19,39 @@ public class Main {
             System.exit(1);
         }
 
-        String search = args[1];
-        String searchMessage = args[2];
+        String patternsFile = args[1];
+        List<String[]> patterns = new ArrayList<>();
+        try(Scanner sc = new Scanner(new File(patternsFile))){
+            while(sc.hasNext()){
+                patterns.add(sc.nextLine().replaceAll("[\"]", "").split(";"));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // reverse the list because of higher priority = higher number
+        Collections.reverse(patterns);
 
         try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Path.of(path))) {
             directoryStream.forEach(p->{
                 if (Files.isRegularFile(p)){
                     // execute the file analyze
                     Thread thead = new Thread(()->{
-                       Analyzer tmp = new Analyzer(search, searchMessage);
-                       tmp.execute(p);
+                        boolean exists = false;
+                        for (String[] pattern: patterns){
+                            try {
+                                if (existsKBPSearch(p,pattern[1], pattern[2])){
+                                    System.out.println(p.getFileName().toString() + ": " + pattern[2]);
+                                    exists = true;
+                                    break;
+                                }
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        if (!exists){
+                            System.out.println(p.getFileName().toString()+": Unknown file type");
+                        }
                     });
                     thead.start();
                     try {
@@ -44,9 +64,18 @@ public class Main {
         } catch (IOException e) {
             System.out.printf("Could not read from directory '%s'!%n", path);
         }
+    }
 
+    private static boolean existsKBPSearch(Path path, String text, String result) throws IOException {
+        String tmp = readFile(path.toString());
+        if (Algorithms.searchKPM(tmp, text)) {
+            return true;
+        }
+        return false;
+    }
 
-
+    private static String readFile(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 
 
